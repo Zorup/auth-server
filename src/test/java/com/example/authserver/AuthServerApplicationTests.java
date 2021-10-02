72748658c21174941cf8aff216edd566c5a017fe
lophunju <lophunju@naver.com>
2021-10-03 03:59:52 +0900

    pushToken Redis로 이동에 따른 수정

--- src/test/java/com/example/authserver/AuthServerApplicationTests.java
+++ src/test/java/com/example/authserver/AuthServerApplicationTests.java
@@ -4,6 +4,7 @@ import com.example.authserver.common.config.RedisConfig;
 import com.example.authserver.domain.Role;
 import com.example.authserver.domain.User;
 import com.example.authserver.dto.UserDto;
+import com.example.authserver.repository.UserJpaRepo;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,12 +15,17 @@ import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 @SpringBootTest
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = RedisConfig.class)
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(classes = RedisConfig.class)
 class AuthServerApplicationTests {
+
+	@Autowired
+	UserJpaRepo userJpaRepo;
 
 	@Autowired
 	RedisTemplate redisTemplate;
@@ -41,42 +47,57 @@ class AuthServerApplicationTests {
 		System.out.println("help me");
 
 		UserDto userDto1 = UserDto.builder()
-				.userId(4L)
-				.name("someone")
+//				.userId(4L)
+				.name("someone2")
 				.role(Role.ROLE_USER)
 				.email("canItWork@naver.com")
 				.build();
 
-		User user = User.of(userDto1);
+//		User user = User.of(userDto1);
+//		User user2 = User.of(userDto1);
+//		userJpaRepo.save(user);
+//		userJpaRepo.save(user2);
 
-		UserDto userDto2 = UserDto.of(user);
+//		UserDto userDto2 = UserDto.of(user);
+//
+//		UserDto.ForRedis userRedisDto = UserDto.ForRedis.of(user);
+//
+////		System.out.println(0);
+////		HashMapping hm = new HashMapping();
+////		System.out.println(1);
+////		hm.write(userRedisDto.getUserId().toString(), userRedisDto);
+////		System.out.println(2);
+////		UserDto.UserRedisDto result = hm.load(userRedisDto.getUserId().toString());
+////		System.out.println(3);
+////		System.out.println(result.getUserId() + " " + result.getRole());
+//
+//
+//		final ValueOperations<String, UserDto.ForRedis> valueOperations = redisTemplate.opsForValue();
+//		valueOperations.set("auth:" + userRedisDto.getUserId().toString(), userRedisDto);
+//		UserDto.ForRedis result = valueOperations.get(prefix + ":" + userRedisDto.getUserId().toString());
+//		System.out.println("check: " + prefix + ":" + userRedisDto.getUserId());
+//		redisTemplate.expire(prefix + ":" + userRedisDto.getUserId(), 30, TimeUnit.SECONDS);
+////		final HashOperations<String, Long, UserDto.UserRedisDto> ho = redisTemplate.opsForHash();
+////		ho.put("auth", userRedisDto.getUserId(), userRedisDto);
+////		UserDto.UserRedisDto result = ho.get("auth", userRedisDto.getUserId());
+//		System.out.println(result == userRedisDto);
+//		System.out.println(userRedisDto.getUserId());
+//		System.out.println(userRedisDto.getRole());
 
-		UserDto.ForRedis userRedisDto = UserDto.ForRedis.of(user);
+		List<String> testList = new ArrayList<>();
+		testList.add("test");
+		testList.add("test2");
 
-//		System.out.println(0);
-//		HashMapping hm = new HashMapping();
-//		System.out.println(1);
-//		hm.write(userRedisDto.getUserId().toString(), userRedisDto);
-//		System.out.println(2);
-//		UserDto.UserRedisDto result = hm.load(userRedisDto.getUserId().toString());
-//		System.out.println(3);
-//		System.out.println(result.getUserId() + " " + result.getRole());
+		final ValueOperations<String, String> ops = redisTemplate.opsForValue();
 
+//		ops.set("test", "testvalue1");
+//		ops.set("test2", "testvalue2");
 
-		final ValueOperations<String, UserDto.ForRedis> valueOperations = redisTemplate.opsForValue();
-		valueOperations.set("auth:" + userRedisDto.getUserId().toString(), userRedisDto);
-		UserDto.ForRedis result = valueOperations.get(prefix + ":" + userRedisDto.getUserId().toString());
-		System.out.println("check: " + prefix + ":" + userRedisDto.getUserId());
-		redisTemplate.expire(prefix + ":" + userRedisDto.getUserId(), 30, TimeUnit.SECONDS);
-//		final HashOperations<String, Long, UserDto.UserRedisDto> ho = redisTemplate.opsForHash();
-//		ho.put("auth", userRedisDto.getUserId(), userRedisDto);
-//		UserDto.UserRedisDto result = ho.get("auth", userRedisDto.getUserId());
-		System.out.println(result == userRedisDto);
-		System.out.println(userRedisDto.getUserId());
-		System.out.println(userRedisDto.getRole());
+		List<String> result = ops.multiGet(testList);
+		System.out.println("result: " + result);
 
-		System.out.println("check return value");
-		System.out.println(redisTemplate.delete("auth:3"));
+//		System.out.println("check return value");
+//		System.out.println(redisTemplate.delete("auth:3"));
 
 	}
 
